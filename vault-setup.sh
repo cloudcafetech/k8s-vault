120836c482cd0c15d05915918f3b91fbfbe4a753
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2021-07-21 16:28:47 +0530

    Create vault-setup.sh

---
+++ vault-setup.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Vault Setup Script KUBERNETES
+
+PUB=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+HIP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
+
+# Install Vault
+if ! command -v vault &> /dev/null;
+then
+ echo "Installing Vault"
+ curl https://releases.hashicorp.com/vault/1.7.3/vault_1.7.3_linux_amd64.zip -o vault_1.7.3_linux_amd64.zip
+ unzip vault_1.7.3_linux_amd64.zip
+ chmod +x vault
+ mv vault /usr/local/bin/vault
+ rm -rf vault_1.7.3_linux_amd64.zip
+fi
+
+# Install Cfssl 
+if ! command -v cfssl &> /dev/null;
+then
+ echo "Installing Cfssl"
+ curl -s -L -o cfssl https://github.com/cloudflare/cfssl/releases/download/v1.6.0/cfssl_1.6.0_linux_amd64
+ chmod +x cfssl
+ mv cfssl /usr/local/bin/
+fi
+
+# Install Cfssljson
+if ! command -v cfssljson &> /dev/null;
+then
+ echo "Installing Cfssljson"
+ curl -s -L -o cfssljson https://github.com/cloudflare/cfssl/releases/download/v1.6.0/cfssljson_1.6.0_linux_amd64
+ chmod +x cfssljson
+ mv cfssljson /usr/local/bin/
+fi
+
+find ./k8s-vault/ -type f -exec sed -i -e "s/172.31.14.138/$HIP/g" {} \;
+find ./k8s-vault/ -type f -exec sed -i -e "s/3.16.154.209/$PUB/g" {} \;
+
+cd k8s-vault
+
+# Certificate Generate
+cfssl gencert -initca certs/config/ca-csr.json | cfssljson -bare certs/ca
+
+cfssl gencert \
+    -ca=certs/ca.pem \
+    -ca-key=certs/ca-key.pem \
+    -config=certs/config/ca-config.json \
+    -profile=default \
+    certs/config/consul-csr.json | cfssljson -bare certs/consul
+
+cfssl gencert \
+    -ca=certs/ca.pem \
+    -ca-key=certs/ca-key.pem \
+    -config=certs/config/ca-config.json \
+    -profile=default \
+    certs/config/vault-csr.json | cfssljson -bare certs/vault
+cp certs/vault.pem certs/tls.crt
+cp certs/vault-key.pem certs/tls.key
+
+# Setup Consul
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm install vault-backend hashicorp/consul -f override/consul-acl.yaml
+
+# setup Vault
+TOKEN=`kubectl get secret vault-backend-consul-bootstrap-acl-token -o template --template '{{.data.token}}'|base64 -d`
+
+#Add this token to Vault override
+sed -i "s/dummytoken/$TOKEN/g" vault/vault.yaml
+
+#Finally install Vault
+kubectl create secret generic vault-vault-cert-active \
+    --from-file=certs/ca.pem \
+    --from-file=certs/tls.crt \
+    --from-file=certs/tls.key
+kubectl create -f vault/vault.yaml
+
+# Setup Ingress
+kubectl create -f vault/ingress.yaml
